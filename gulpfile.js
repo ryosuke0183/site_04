9b119bdba4be4186d8dfe2d47daa290980676afd
ryosuke0183 <r.simon0000g@gmail.com>
2023-05-26 08:47:55 +0900

    code pug

--- gulpfile.js
+++ gulpfile.js
@@ -17,55 +17,60 @@ import webpack from 'webpack';
 import webpackConfig from './webpack.config.js';
 
 const pugTask = () => {
-    return gulp.src(['./src/pug/**/*.pug', '!./src/pug/**/_*.pug'])
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true,
-            basedir: './src/pug'
-        }))
-        .pipe(gulp.dest('./dist'))
-}
-
-const sassTask = () => {
-    return gulp.src(['./src/scss/**/*.scss', '!./src/scss/**/_*.scss'])
+    return gulp
+        .src(['./src/pug/**/*.pug', '!./src/pug/**/_*.pug'])
         .pipe(plumber())
         .pipe(
-            sass.sync({
-                outputStyle: 'expanded'
-            }).on('error', sass.logError)
+            pug({
+                pretty: '    ',
+                basedir: './src/pug',
+            })
+        )
+        .pipe(gulp.dest('./dist'));
+};
+
+const sassTask = () => {
+    return gulp
+        .src(['./src/scss/**/*.scss', '!./src/scss/**/_*.scss'])
+        .pipe(plumber())
+        .pipe(
+            sass
+                .sync({
+                    outputStyle: 'expanded',
+                })
+                .on('error', sass.logError)
         )
         .pipe(cssLint())
         .pipe(prefixer())
-        .pipe(gulp.dest('./dist/css'))
-}
+        .pipe(gulp.dest('./dist/css'));
+};
 
 const bundleJSTask = () => {
-    return webpackStream(webpackConfig, webpack)
-        .pipe(gulp.dest('./dist/js'));
-}
+    return webpackStream(webpackConfig, webpack).pipe(gulp.dest('./dist/js'));
+};
 
 const serverInit = () => {
     browsersync({
         server: {
             baseDir: './dist',
             index: 'index.html',
-            directory: true
+            directory: true,
         },
         startPath: '/index.html',
-        notify: false
-    })
-}
+        notify: false,
+    });
+};
 
 const reloadTask = (cb) => {
     browsersync.reload();
     cb();
-}
+};
 
 const watchTask = () => {
     gulp.watch('./src/scss/**/*.scss', sassTask);
     gulp.watch('./src/pug/**/*.pug', pugTask);
     gulp.watch('./src/js/**/*.js', bundleJSTask);
     gulp.watch('./src/**/*', reloadTask);
-}
+};
 
 export default gulp.parallel(watchTask, serverInit);
